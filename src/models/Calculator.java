bf70098b1922e8b010622699aadb0176c06fc72e
Santiago Rojas <manuel.chaparro01@uptc.edu.co>
2022-09-04 02:56:34 -0500

    Creacion de metodo suma y resta

---
+++ src/models/Calculator.java
@@ -0,0 +1,12 @@
+package models;
+
+public class Calculator {
+
+    public int sum(int num1, int num2){
+        return num1+num2;
+    }
+
+    public int substract(int num1, int num2){
+        return num1-num2;
+    }
+}
