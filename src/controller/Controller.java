d38572809a5224ee2812da94c778644e73b97dae
Santiago Rojas <manuel.chaparro01@uptc.edu.co>
2022-09-04 02:46:44 -0500

    Creacion de repositorio local

---
+++ src/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+
+public class Controller {
+
+    public static void main(String[] args) {
+        new Controller();
+    }
+
+    public Controller(){
+        System.out.println("HolaMundo");
+    }
+}
