03aaa29e2925f7ef45bfca8b4d1e7494834524d2
Luis Sandoval <luis.sandoval03@uptc.edu.co>
2022-09-05 11:28:45 -0500

    Adición metodos de multiplicar y dividir, de interfaz por consola y MVC

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,12 +1,58 @@
 package controller;
 
+import models.Calculator;
+import views.Views;
+
+/**
+ * Esta clase define los metodos que reunen a los modelos y a las vistas. 
+ * @author: Manuel C. Rojas, Luis S. Parra, Pedro J. Neira
+ * @version:  05/09/2022/A
+ */
 public class Controller {
+	
+	private Calculator calculator;
+	private Views views;
 
-    public static void main(String[] args) {
-        new Controller();
-    }
-
+	 /**
+     * Constructor para el controlador de la aplicacion.
+     */
     public Controller(){
-        System.out.println("HolaMundo");
+    	calculator = new Calculator();
+    	views = new Views();
+    	init();
+    }
+    
+    /**
+     * Metodo que ejecuta el menú de funcionalidades de la calculadora, uniendo modelos y vistas.
+     */
+	private void init() {
+		boolean isRun = false;
+		while (!isRun) {
+			switch (views.showMenu()) {
+			case 1:
+				views.viewResult("Suma", calculator.sum(views.inputNumOne(), views.inputNumTwo()));
+				break;
+			case 2:
+				views.viewResult("Resta", calculator.substract(views.inputNumOne(), views.inputNumTwo()));
+				break;
+			case 3:
+				views.viewResult("Multiplicación", calculator.multiplication(views.inputNumOne(), views.inputNumTwo()));
+				break;
+			case 4:
+				views.viewResult("División", (int)(calculator.split(views.inputNumOne(), views.inputNumTwo())));
+				break;
+			case 5:
+				System.exit(0);
+				break;
+			default:
+				views.inputErr();
+				break;
+			}		
+		}
+		System.exit(0);
+	}   
+
+	public static void main(String[] args) {
+        new Controller();
     }
 }
